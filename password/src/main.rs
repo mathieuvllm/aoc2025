4c79d186ab736544e22e658dc88f69419a9a0a4b
Mathieu Villaume <mathieu.villaume@grenoble-inp.org>
2025-12-01 22:21:19 +0100

    day 1 part 2

--- password/src/main.rs
+++ password/src/main.rs
@@ -1,15 +1,23 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn rotate(current: i32, distance: i32) -> i32 {
+fn rotate(current: i32, distance: i32) -> (i32, u32) {
     let mut sum = current + distance;
+    let mut zeroes: u32 = 0;
     while sum < 0 {
         sum += 100;
+        if current != 0 || sum < 0 {
+            zeroes += 1;
+        }
     }
     while sum > 99 {
         sum -= 100;
+        if sum != 0 {
+            zeroes += 1;
+        }
     }
-    sum
+
+    (sum, zeroes)
 }
 
 fn parse(path: &str) -> io::Result<Vec<i32>> {
@@ -41,10 +49,12 @@ fn main() {
 
     let mut current = 50;
     for &distance in input.iter() {
-        current = rotate(current, distance);
+        let ret = rotate(current, distance);
+        current = ret.0;
         if current == 0 {
             password += 1;
         }
+        password += ret.1;
     }
     println!("{password}");
 }
