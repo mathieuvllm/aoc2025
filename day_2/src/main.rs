3ac4fd294457bfe664903145c4ab492afa7a30e0
Mathieu Villaume <mathieu.villaume@grenoble-inp.org>
2025-12-02 20:22:23 +0100

    part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -10,12 +10,42 @@ fn is_valid(id: u128) -> bool {
     parts.0 != parts.1
 }
 
-fn invalid_in_range(first: u128, last: u128) -> Option<Vec<u128>> {
+fn is_valid_2(id: u128) -> bool {
+    let id = id.to_string();
+    let length = id.len();
+
+    for n in 2..=length {
+        if id.len().is_multiple_of(n) {
+            let slice_size = length / n;
+            if id
+                .as_bytes()
+                .chunks(slice_size)
+                .all(|chunk| chunk == &id.as_bytes()[..slice_size])
+            {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+fn invalid_in_range(first: u128, last: u128, part: u8) -> Option<Vec<u128>> {
     let mut invalid = vec![];
 
     for id in first..=last {
-        if !(is_valid(id)) {
-            invalid.push(id);
+        match part {
+            1 => {
+                if !(is_valid(id)) {
+                    invalid.push(id);
+                }
+            }
+            2 => {
+                if !(is_valid_2(id)) {
+                    invalid.push(id);
+                }
+            }
+            _ => return None,
         }
     }
 
@@ -46,7 +76,7 @@ fn main() {
 
     let mut total: u128 = 0;
     for &(first, last) in ranges.iter() {
-        if let Some(invalid) = invalid_in_range(first, last) {
+        if let Some(invalid) = invalid_in_range(first, last, 2) {
             let sum: u128 = invalid.iter().sum();
             total += sum;
         }
