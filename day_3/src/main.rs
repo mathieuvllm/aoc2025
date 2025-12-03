5b2d29e0fb65e4ccca736b1b81eaa7b4cd1c383b
Mathieu Villaume <mathieu.villaume@grenoble-inp.org>
2025-12-03 17:05:43 +0100

    part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -15,7 +15,29 @@ fn parse(path: &str) -> io::Result<Vec<Vec<u8>>> {
     Ok(banks)
 }
 
+fn max_index(bank: Vec<u8>) -> usize {
+    bank.iter()
+        .enumerate()
+        .reduce(|(index1, value1), (index2, value2)| {
+            if value2 > value1 {
+                (index2, value2)
+            } else {
+                (index1, value1)
+            }
+        })
+        .unwrap()
+        .0
+}
+
+fn max_joltage(bank: Vec<u8>) -> u32 {
+    let a = max_index(bank[..bank.len() - 1].to_vec());
+    let b = max_index(bank[a + 1..].to_vec()) + a + 1;
+
+    (bank[a] * 10 + bank[b]) as u32
+}
+
 fn main() {
-    let input = parse("example.txt").ok().unwrap();
-    println!("{input:?}");
+    let input = parse("input.txt").ok().unwrap();
+    let sum: u32 = input.iter().map(|v| max_joltage(v.clone())).sum();
+    println!("{sum}");
 }
