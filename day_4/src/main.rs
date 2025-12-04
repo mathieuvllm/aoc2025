28aa55cd156bd8e689bf5397fbd77c61a636b668
Mathieu Villaume <mathieu.villaume@grenoble-inp.org>
2025-12-04 14:50:08 +0100

    day 4 parse

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn parse(path: &str) -> io::Result<Vec<Vec<char>>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut rows: Vec<Vec<char>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        rows.push(line.chars().collect());
+    }
+
+    Ok(rows)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = parse("example.txt").ok().unwrap();
+    println!("{input:?}");
 }
